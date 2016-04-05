4006730439c3ed1c4846a46eee3e41c38e0a131d
Ramatoulie Sanyang <misramz@yahoo.com>
2016-04-05 13:45:25 -0400

    in class work on JS functions and how they work

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var firstName = 'Nisa';
+var lastName = 'Sanyang';
+var name = firstName + ' ' + lastName;
+
+console.log(name);
+
+function greet () {
+  var text = "Good Morning, " + name;
+  console.log(text);
+}
